139be727e1ab78bd15c54c439ac6c827e947b908
DESKTOP-P3HDN72\Florian <florian.kittelmann@openbyte.ch>
2022-11-09 09:52:16 +0100

    rotate around object

--- eval/cameras/rotate.py
+++ eval/cameras/rotate.py
@@ -4,10 +4,13 @@
 # This source code is licensed under the license found in the
 # LICENSE file in the root directory of this source tree.
 #
+import math
+
 import numpy as np
 
 import torch.utils.data
 from data.CameraSetups.CameraSetupInBlender2 import CameraSetupInBlender2
+from scipy.spatial.transform import Rotation as R
 
 
 class Dataset(torch.utils.data.Dataset):
@@ -34,18 +37,28 @@ class Dataset(torch.utils.data.Dataset):
 
     def __getitem__(self, idx):
         # t = (np.cos(idx * 2. * np.pi / self.period) * 0.5 + 0.5)
-        campos = self.camera.get_cam_pos_training().astype(np.float32)
 
-        """lookat = np.array([0., 0., 0.], dtype=np.float32)
-        up = np.array([0., 0., 1.], dtype=np.float32)
-        forward = lookat - campos
-        forward /= np.linalg.norm(forward)
-        right = np.cross(up, forward)
-        right /= np.linalg.norm(right)
-        up = np.cross(forward, right)
-        up /= np.linalg.norm(up)"""
+        nof_frames = 500.0
+        radius = self.camera.get_radius()
+        step = 2.0 * math.pi / nof_frames
+        alpha = idx * step
+        x = math.cos(alpha) * radius
+        y = math.sin(alpha) * radius
+        z = 0.0
 
-        camrot = self.camera.get_cam_rot_matrix_training().astype(np.float32)
+        z_rot = alpha + 90
+        xyz_rot = [0.0, 0.0, z_rot]
+        xyz_rot = R.from_euler('xyz', xyz_rot, degrees=True)
+        extrinsic_matrix = np.array(xyz_rot.as_matrix()).astype(np.float32)
+        rad_rot = 180.0 / 360.0 * 2 * math.pi
+        y_rot_matrix = np.asarray([
+            [math.cos(rad_rot), 0, math.sin(rad_rot)],
+            [0, 1, 0],
+            [-math.sin(rad_rot), 0, math.cos(rad_rot)],
+        ])
+        camrot = extrinsic_matrix.dot(y_rot_matrix).T.astype(np.float32)
+        xyz_pos = [x, y, z]
+        campos = np.array(xyz_pos).astype(np.float32)
 
         px, py = np.meshgrid(np.arange(self.width).astype(np.float32), np.arange(self.height).astype(np.float32))
         pixelcoords = np.stack((px, py), axis=-1)
