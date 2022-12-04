84ee6ef82e018b1c31945fb9659614b97d6016bd
DESKTOP-P3HDN72\Florian <florian.kittelmann@openbyte.ch>
2022-12-04 19:13:04 +0100

    changes

--- config_templates/blender2_config.py
+++ config_templates/blender2_config.py
@@ -50,7 +50,7 @@ class DatasetConfig:
         return TrainBlender2(
             get_autoencoder_func=self.get_autoencoder_config_func,
             get_dataset_func=self.get_dataset_config_func,
-            batchsize=32,
+            batchsize=16,
             maxiter=500000,
             lr=0.0001
         )
